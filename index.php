1d5d701960495442c7b846aa59fca3c8c9eae1d7
Thomas McMahon <tmcmahon@nerdery.com>
2016-09-06 16:36:39 -0500

    First Code Check-in
    This is the initial plugin files.

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+* Plugin Name: Beaver Builder Menu Polish
+* Plugin URI: http://www.twistermc.com/
+* Description: Adding drop down menu indicators and search to mobile.
+* Version: 0.1
+* Author: Thomas McMahon
+* Author URI: http://www.twistermc.com/
+* License: A "Slug" license name e.g. GPL12
+*/
+
+/**
+ * Add Search Box to Mobile
+ * Adds a search box to mobile, below the header.
+ */
+function bbMenuPolish_addMobileSearch(){
+    if ( wp_is_mobile() ) {
+        get_search_form();
+    }
+}
+
+add_action( 'fl_after_header', 'bbMenuPolish_addMobileSearch' );
+
+/**
+ * Add Custom CSS 
+ * For Search and Dropdown Arrows
+ */
+
+function bbMenuPolish_AddCSS() {
+    // Respects SSL, Style.css is relative to the current file
+    wp_register_style( 'bbMenuPolish-css', plugins_url('style.css', __FILE__) );
+    wp_enqueue_style( 'bbMenuPolish-css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'bbMenuPolish_AddCSS' );
